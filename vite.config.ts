ba9a31bcad29eee37f57591fa1c47092a3b27510
yaakinnn <224627367+yaakinnn@users.noreply.github.com>
2026-05-05 12:17:22 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -20,5 +20,6 @@ export default defineConfig(({mode}) => {
       // Do not modifyâfile watching is disabled to prevent flickering during agent edits.
       hmr: process.env.DISABLE_HMR !== 'true',
     },
+    base: '/portfolio/',
   };
 });
